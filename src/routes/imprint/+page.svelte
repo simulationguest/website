8bb230ec1f3b88e030c4f99a9f979eb8d3b4b931
Leopold Jofer <projekte@leoj.de>
2024-09-29 10:01:34 +0200

    add imprint

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	import Wrapper from '$lib/Wrapper.svelte';
+</script>
+
+<Wrapper title="Imprint">
+	<main class="flex flex-col gap-4">
+		<p>Information according to German §5 of TMG</p>
+
+		<p>
+			Leo Jofer <br />
+			Heisenbergstraße 7<br />
+			85748 Garching bei München <br />
+			Germany
+		</p>
+
+		<p><a href="mailto:contact@leoj.de" class="link">contact@leoj.de</a></p>
+
+		<h2>Copyright</h2>
+
+		<p>
+			This website and its contents are subject to German copyright law. Unless expressly permitted
+			by law (§ 44a et seq. of the copyright law), every form of utilizing, reproducing, or
+			processing the design of this site, or its containing works, is subject to copyright
+			protection and requires my prior consents.
+		</p>
+
+		<p>
+			Individual reproductions of a work are allowed only for private use, so must not serve either
+			directly or indirectly for earnings. Unauthorized utilization of copyrighted works is
+			punishable (§ 106 of the copyright law).
+		</p>
+
+		<h2>Limitation of liability for internal content</h2>
+
+		<p>
+			The contents of this website have been created with my utmost care. However, I cannot
+			guarantee the contents' accuracy, completeness, or topicality. According to statutory
+			provisions, I'm furthermore responsible for my own content. In this context, please note that
+			I'm accordingly not obliged to monitor merely the transmitted or saved information of third
+			parties, or investigate circumstances pointing to illegal activity.
+		</p>
+
+		<p>
+			My obligation to remove or block the use of information under generally applicable laws remain
+			unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
+		</p>
+	</main>
+</Wrapper>
+
+<style>
+	h2 {
+		@apply mt-4 text-xl;
+	}
+</style>
