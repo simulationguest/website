adb224005c3354ab867c0da03ff79cc97f96b559
Leopold Jofer <projekte@leoj.de>
2024-09-30 11:51:30 +0200

    dark logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 <Meta title="" description="">
 	<div class="w-full min-h-[80vh] grid place-items-center">
 		<main class="text-center flex flex-col gap-4 items-center">
-			<img src={Logo} alt="leoj.de" class="text-xl" />
+			<img src={Logo} alt="leoj.de" class="text-xl dark:invert" />
 			{#each Object.entries(NavList) as [href, label]}
 				<a {href} class="text-4xl mt-2 hover:bg-black hover:text-white hover:italic block"
 					>{label}</a
