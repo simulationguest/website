9fc08333d5082e6fd964e98d245bc7c1a6cc35c4
Leopold Jofer <projekte@leoj.de>
2024-10-01 02:09:12 +0200

    remove dirs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "never",
+  build: {
+    format: "file",
+  },
   integrations: [tailwind()],
 });
